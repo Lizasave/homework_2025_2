595ada6723c20578c88adda1470bb5fb56446040
Elizaveta Saveleva <e.farafonova@corp.mail.ru>
2025-09-08 10:09:57 +0300

    e.saveleva homework 0 - done

---
+++ source/sortByLength.js
@@ -0,0 +1,21 @@
+/**
+ * Функция, определяющая максимальное число в массиве
+ * @param {Array<String>} strings - массив строк
+ *
+ * @example
+
+ * // returns ["ant", "bat", "cat", "dog"]
+ * sortByLength(["cat", "bat", "ant", "dog"])
+ *
+ * @returns {Array<String>}
+
+ */
+const sortByLength = strings => {
+    let arrSort = strings.slice();
+    return arrSort.sort((a,b) => {
+        if (a.length === b.length) {
+            return a.localeCompare(b);
+        }
+        return a.length - b.length;
+    })
+}
